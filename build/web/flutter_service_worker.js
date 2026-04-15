63cf7e3949ef9760a3b1984fa598f355e7b69f41
Hasnain <patelhusnain02@gmail.com>
2026-04-15 20:10:02 +0530

    Z

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f6e4389efcbe7363df1e2258e051c14e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a5fbe496667e22b750b7a0137c7b0050",
+"flutter_bootstrap.js": "0cdf469c27827404f3a52ea126f19c00",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
